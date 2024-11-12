ad1426ce10855f273af70e79474d801d9dea6fec
Tahiru Agbanwa <130235676+squirelboy360@users.noreply.github.com>
2024-11-12 17:45:01 +0100

    solved

--- files.py
+++ files.py
@@ -2,14 +2,15 @@ ASCII = "ascii"
 UTF_8 = "utf-8"
 UTF_16 = "utf-16"
 
-def read_file(filename, en="utf-8"):
+def read_file(filename, en=UTF_8):
     output = ""
     with open(filename, encoding=en) as fin:
         for line in fin:
             output += line
     return output
 
-def write_file(filename, contents, en="utf-8"):
+
+def write_file(filename, contents, en=UTF_8):
     with open(filename, "w", encoding=en) as fout:
         fout.write(contents)
 
@@ -18,3 +19,22 @@ def write_html_file(filename, content):
         fout.write("<html>")
         fout.write(content)
         fout.write("</html>")
+
+text = read_file("input.txt", UTF_8)
+
+
+write_file("text_in_UTF_16.txt", text, UTF_16)
+
+with open("code_points.txt", "w") as code_points_file:
+    for char in text:
+        code_point = f"U+{ord(char):04X}" 
+        code_points_file.write(code_point + "\n")
+
+html_content = ""
+for char in text:
+    code_point = f"&#{ord(char)};" 
+    html_content += code_point
+
+write_html_file("text_in_HTML.html", html_content)
+
+print("Process completed. Files have been generated as required.")
